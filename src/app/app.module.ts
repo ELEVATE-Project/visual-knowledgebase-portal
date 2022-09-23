09d2d1e9b07c78d079c0a32fb66eb22c1ba64323
5Amogh <amoghavarsh@navadhiti.com>
2022-09-23 18:38:33 +0530

    Banner,Search,Global styles, Blog(in-progress)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { HeaderComponent } from './components/shared/header/header.component';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 import { BannerComponent } from './components/shared/banner/banner.component';
 import { HomeComponent } from './components/home/home.component';
 import { CardComponent } from './components/shared/card/card.component';
 import { BlogComponent } from './components/blog/blog.component';
+import { SearchComponent } from './components/shared/search/search.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,17 +27,22 @@ import { BlogComponent } from './components/blog/blog.component';
     BannerComponent,
     HomeComponent,
     CardComponent,
-    BlogComponent
+    BlogComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatToolbarModule,
     MatIconModule,
     MatCardModule,
     MatMenuModule,
+    MatInputModule,
     MatFormFieldModule,
+    MatStepperModule,
     CKEditorModule
   ],
   providers: [],
--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-
+import {FormBuilder, Validators, FormArray} from '@angular/forms';
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
@@ -8,10 +8,36 @@ import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 })
 export class BlogComponent implements OnInit {
   public Editor = ClassicEditor;
-  constructor() { }
+  public blog = ''
+  fileName:string = 'Choose images to upload icon (svg, jpg, png)'
+  firstFormGroup = this._formBuilder.group({
+    categoryName: ['', Validators.required],
+    categoryDescription:['',Validators.required],
+    subCategories:this._formBuilder.array([])
+  });
+
+  get subCategories() : FormArray {
+    return this.firstFormGroup.get("subCategories") as FormArray
+  }
+
+  getSubCategories(){
+    // const subCategory = this._formBuilder.group({
+    //   name: new FormControl(''),
+    //   blog: new FormControl('')
+    // });
+    this.subCategories.push(this._formBuilder.control(''))
+  }
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     
   }
 
+  iconChoosen($event:any){
+    const file = $event.target.files[0];
+    if (['image/jpeg','image/png','image/svg+xml'].includes(file.type)){
+      this.fileName = file.name;
+      console.log($event)
+    }
+  }
 }
