fca2da011172e94e13c68aea598d53c48da9860c
im ranjitha <ranjithatk101@gmail.com>
2022-10-25 11:22:48 +0530

    issue #feat api-integration

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,99 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { ApiserviceService } from '../service/apiservice.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  categories = [
-    {
-      icon: 'group',
-      name: 'About us',
-      description:
-        'This is a sample description of the card contanining two lines of description',
-    },
-    {
-      name: 'Account',
-      description:
-        'This is a sample description of the card contanining two lines of description',
-      icon: 'person',
-      blog: '',
-      children: [
-        {
-          name: 'Login',
-          children: [
-            {
-              parent: 'Login',
-              name: 'Forgot password',
-            },
-            {
-              name: 'Two-factor authentication',
-            },
-          ],
-        },
-        { name: 'Profile' },
-      ],
-    },
-    {
-      icon: 'admin_panel_settings',
-      name: 'Administration',
-      description:
-        'This is a sample description of the card contanining two lines of description',
-    },
-    {
-      icon: 'analytics',
-      name: 'Analytics',
-      description:
-        'This is a sample description of the card contanining two lines of description',
-    },
-    {
-      icon: 'forum',
-      name: 'Community',
-      description:
-        'This is a sample description of the card contanining two lines of description',
-    },
-    {
-      icon: 'engineering',
-      name: 'Engineering',
-      description:
-        'This is a sample description of the card contanining two lines of description',
-    },
-    {
-      icon: 'help',
-      name: 'Support',
-      description:
-        'This is a sample description of the card contanining two lines of description',
-    },
-    {
-      icon: 'shopping_cart',
-      name: 'Products',
-      description:
-        'This is a sample description of the card contanining two lines of description',
-    },
-    {
-      icon: 'emoji_objects',
-      name: 'Solution',
-      description:
-        'This is a sample description of the card contanining two lines of description',
-    },
-    {
-      icon: 'calendar_month',
-      name: 'Events',
-      description:
-        'This is a sample description of the card contanining two lines of description',
-    },
-    {
-      icon: 'calendar_month',
-      name: 'Abcd',
-      description: 'abcd',
-      _id: '634d2fb497e47b1b7127e51b',
-    },
-  ];
-  constructor(private http: HttpClient) {}
-
-  ngOnInit(): void {}
+  constructor(
+    private http: HttpClient,
+    private apiService: ApiserviceService
+  ) {}
+  categories: any;
+  ngOnInit(): void {
+    this.getAllCategory();
+  }
 
   newCategory($event: any) {
     console.log('new cat', $event);
@@ -106,5 +27,10 @@ export class HomeComponent implements OnInit {
       .subscribe((data) => {
         console.log(data);
       });
+  }
+  getAllCategory() {
+    this.apiService
+      .getAllCategory()
+      .subscribe((data) => (this.categories = data.result));
   }
 }
