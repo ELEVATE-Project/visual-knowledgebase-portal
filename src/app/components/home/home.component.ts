f6537dbc80f7b953d2b184295374b828ade8d1c7
im ranjitha <ranjithatk101@gmail.com>
2022-10-25 19:57:14 +0530

    #api-integration-get-postApi

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ApiserviceService } from '../service/apiservice.service';
+import { ApiService, ToastService } from 'src/app/core/service';
+import { urlConstants } from 'src/app/core/constants/urlconstants';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,7 +12,9 @@ import { ApiserviceService } from '../service/apiservice.service';
 export class HomeComponent implements OnInit {
   constructor(
     private http: HttpClient,
-    private apiService: ApiserviceService
+    private apiService: ApiserviceService,
+    private toastService: ToastService,
+    private apiService1: ApiService
   ) {}
   categories: any;
   ngOnInit(): void {
@@ -17,20 +22,33 @@ export class HomeComponent implements OnInit {
   }
 
   newCategory($event: any) {
-    console.log('new cat', $event);
-    this.categories.push($event.data);
-    this.http
-      .post('http://34.126.99.183/knowledgebase/v1/topics/create', {
+    let createCategory = {
+      url: urlConstants.createTopic,
+      payload: {
         topicName: $event.data.name,
         description: $event.data.description,
-      })
-      .subscribe((data) => {
-        console.log(data);
-      });
+      },
+    };
+    this.apiService1.post(createCategory).subscribe((data) => {
+      this.toastService.showMessage(data.message, 'success');
+      this.getAllCategory();
+    });
   }
+
   getAllCategory() {
-    this.apiService
-      .getAllCategory()
-      .subscribe((data) => (this.categories = data.result));
+    let categories = {
+      url: urlConstants.getTopics,
+    };
+    this.apiService1.get(categories).subscribe((data) => {
+      console.log(data);
+      if (data && data.result) {
+        this.categories = data.result;
+      } else {
+        this.toastService.showMessage(
+          data.message,
+          'no data available to display'
+        );
+      }
+    });
   }
 }
