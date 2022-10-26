a8a29719a8f4a988b3a878748dc24b8ab3907801
im ranjitha <ranjithatk101@gmail.com>
2022-10-26 11:49:43 +0530

    api-integration

--- src/app/core/constants/urlconstants.ts
+++ src/app/core/constants/urlconstants.ts
@@ -1,5 +1,3 @@
 export const urlConstants = {
-  login: 'user/v1/systemUsers/login',
-  getTopics: 'knowledgebase/v1/topics/read',
-  createTopic: 'knowledgebase/v1/topics/create',
-};
+login:'user/v1/systemUsers/login'
+}
--- src/app/core/service/interceptor/apiInterceptor.ts
+++ src/app/core/service/interceptor/apiInterceptor.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor,
+    HttpRequest,
+    HttpHandler,
+    HttpEvent,
+    HttpInterceptor
 } from '@angular/common/http';
 import { Observable, from } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -11,33 +11,34 @@ import { CurrentUserService } from '../currentUser/current-user.service';
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
-  constructor(private userService: CurrentUserService) {}
-  getToken() {
-    return this.userService.getAccessToken().then((token) => {
-      let accessToken: any = JSON.parse(token);
-      return accessToken.access_token;
-    });
-  }
-  intercept(
-    request: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    return from(this.handle(request, next)) as Observable<HttpEvent<any>>;
-  }
-
-  async handle(req: HttpRequest<any>, next: HttpHandler) {
-    let authReq;
-    if (req.url != environment.baseUrl + 'user/v1/systemUsers/login') {
-      const token: any = await this.getToken();
-      authReq = req.clone({
-        setHeaders: {
-          'X-auth-token': 'bearer ' + token,
-        },
-      });
-      return next.handle(authReq).toPromise();
-    } else {
-      authReq = req.clone({});
+    constructor(
+        private userService: CurrentUserService
+    ) {
     }
-    return next.handle(authReq).toPromise();
-  }
+    getToken(){
+        return this.userService.getAccessToken().then(token =>{
+            let accessToken:any =  JSON.parse(token);
+            return accessToken.accessToken;
+        })
+    }
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return from(this.handle(request, next))  as Observable<HttpEvent<any>>;
+    }
+
+    async handle(req: HttpRequest<any>, next: HttpHandler) {
+        let authReq;
+        if (req.url != environment.baseUrl + "user/v1/systemUsers/login") {
+            const token :any =  await this.getToken();
+            authReq = req.clone({
+                setHeaders: {
+                    'X-auth-token':'bearer '+ token,
+                }
+            });
+        return next.handle(authReq).toPromise()
+        } else {
+            authReq = req.clone({
+            })
+        }
+        return next.handle(authReq).toPromise()
+    }
 }
